399bbc4297fbc260e7c702b7b45928a01568c733
ccc <2093585357@qq.com>
2025-08-20 23:14:02 +0800

    方案展示部分1

--- app/page.js
+++ app/page.js
@@ -7,12 +7,16 @@ import PostItem from "@/components/PostItem";
 
 const PostList = ({posts}) => { 
   return (
+    <div>
+       <h1 className={styles.sectionTitle}>满足各场需求的定制化方案</h1>
     <div className={styles.postList}>
       {posts.map(item=>{
-    const {id,title,content,tags} = item;
-    return <PostItem key={id} title={title} content={content} tags={tags} />
+    const {id,title,content,tags,imgUrl} = item;
+    return <PostItem key={id} title={title} content={content} tags={tags} imgUrl={imgUrl} />
   })}
     </div>
+    </div>
+    
   )
 };
 
@@ -25,54 +29,58 @@ export default function Home() {
       title: "工厂",
       content: "This is the content of Plan 1.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-01.jpg'
     },
     {
       id: 2,
-      title: "Plan 2",
+      title: "商场",
       content: "This is the content of Plan 2.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-02.jpg'
     },
     {
       id: 3,
-      title: "Plan 3",  
+      title: "医院",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-03.jpg'
     },
     {
       id:4 ,
-      title: "Plan 4",  
+      title: "图书馆",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-04.jpg'
     },{
       id: 5,
-      title: "Plan 5",  
+      title: "学校",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-05.jpg'
     },{
       id: 6,
-      title: "Plan 6",  
+      title: "机场",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-06.jpg'
     },{
       id: 7,
-      title: "Plan 7",  
+      title: "法院",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-07.jpg'
     },{
       id: 8,
-      title: "Plan 8",  
+      title: "会展",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-08.jpg'
     },{
       id: 9,
-      title: "Plan 9",  
+      title: "公园",  
       content: "This is the content of Plan 3.",
       tags: ["工厂", "生产", "制造"],
-    },{
-      id: 10,
-      title: "Plan 10",  
-      content: "This is the content of Plan 3.",
-      tags: ["工厂", "生产", "制造"],
+      imgUrl:'/postImg/post-item-img-09.jpg'
     },
 
   ]
--- components/PostItem/index.tsx
+++ components/PostItem/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from './index.module.css';
-export default function PostItem({title}){
+import Image from 'next/image';
+export default function PostItem({title,imgUrl}){
   return (
-    <div className={styles.postItem}> {title} </div>
+    <div className={styles.postItem}> 
+    <div className={styles.imageWrapper}>
+      <Image  src={imgUrl} fill />
+    </div>
+    <div className={styles.content}>
+      <h3 className={styles.postTitle}>{title} </h3>
+    </div>
+    </div>
   );
 }
